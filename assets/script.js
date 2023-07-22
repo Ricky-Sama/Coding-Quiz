70930b1ea1b5e7d259fdf73fc92b6136145a7789
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-07-22 17:43:48 -0400

    added questions and answers to js file

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Quiz Questions
+const question = [
+    {
+      question: "Commonly used data types DO not include:",
+      answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+      correctAnswer: "Alerts"
+    },
+    {
+      question: "The condition in an if/else statement is enclosed with what?",
+      answers: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+      correctAnswer: "Parenthesis"
+    },
+    {
+      question: "Arrays in JavaScript can be used to store:",
+      answers: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+      correctAnswer: "All of the above"
+      },
+    {
+      question: "String values must be enclosed within ______ when being assigned to variables.",
+      answers: ["Commas", "Curly Brackets", "Quotes", "Paranthesis"],
+      correctAnswer: "Alerts"
+    },  
+];
